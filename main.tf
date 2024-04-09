1cfca131055a816bbfe1d7546d68cc6f0a8207d7
Strawhataakash <142259216+Strawhataakash@users.noreply.github.com>
2024-04-09 19:01:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@ provider "aws" {
   profile = "myaws"
   region  = var.region
 }
-variable "region" {
-  description = "AWS region where resources will be deployed"
-  type        = string
-  default     = "ap-east-1"  # Provide a default value if needed
-}
-
 resource "aws_s3_bucket" "b" {
   bucket = "myawsbucket1265555555555555"
  
